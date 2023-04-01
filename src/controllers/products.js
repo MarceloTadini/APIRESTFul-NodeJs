f94069b71cfab80aaa1ac56f219849c0f298bfa1
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-04-01 18:19:53 -0300

    getting the products id

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,8 +1,12 @@
 const ProductsModel = require('../models/products')
 
 async function get(req, res) {
+
+    const {id} = req.params
+
+    const obj = id ? { _id: id } : {}
     
-    const products = await ProductsModel.find()
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
 }
